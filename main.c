0f1fe363184c084032264d696800e18581a5bd5d
maja.korajcevic <maja.korajcevic@check24.de>
2021-10-21 10:18:45 +0200

    Add main

---
+++ main.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main()
+{
+	int i = 0;
+	char s[50];
+	printf("Enter String : ");
+	gets(s);
+	while (s[i] != '\0')
+	{
+		if (s[i] == 'a')
+		{
+			s[i] = '*';
+		}
+		i++;
+	}
+
+	printf("-------------------------------------");
+	printf("\nString After Replacing 'a' by '*'");
+	printf("\n-------------------------------------\n");
+	printf("%s", s);
+	printf("-------------------------------------");
+	return 0;
+}
