4c6115ee4fe325f73910e8af92d2ec27719cea1e
Maja Korajcevic <maja.korajcevic@check24.de>
2021-10-23 11:19:19 +0200

    Revert "change message"
    This reverts commit cf3a0dab24bd5f3e79b3df761030bc1942cfa4ec.

--- main.c
+++ main.c
@@ -16,7 +16,7 @@ int main()
 	}
 
 	printf("-------------------------------------");
-	printf("\nNew string 'a' by '*'");
+	printf("\nString After Replacing 'a' by '*'");
 	printf("\n-------------------------------------\n");
 	printf("%s", s);
 	printf("-------------------------------------");
